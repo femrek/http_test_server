70b0de9adfcc7633a29cdaa3213e141750741089
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-01-09 06:28:58 +0300

    refactor: example project improved.
    * added a sample response model to convert json to object.

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http_test_server/http_test_server.dart';
@@ -6,24 +7,37 @@ import 'package:test/test.dart';
 void main() {
   test('test', () async {
     // define test values
-    const eResponse = 'Hello, world!';
+    const message = 'Hello, World!';
 
     final server = await TestServer.createHttpServer(events: [
       StandardServerEvent(
         matcher: ServerEvent.standardMatcher(paths: ['/']),
-        handler: (request) => eResponse,
+        handler: (request) => '{"message": "$message"}',
       ),
     ]);
+    final url = 'http://localhost:${server.port}';
 
     // Send a request to the server
-    final response = await _request('http://localhost:${server.port}');
+    final response = await _request(url);
+    final responseModel = ResponseModel.fromJson(response);
 
-    // Expect the response
-    expect(response, eResponse);
+    // Expectations
+    expect(responseModel.message, message);
 
     // Close the server
     await server.close(force: true);
   });
+}
+
+class ResponseModel {
+  ResponseModel({required this.message});
+
+  factory ResponseModel.fromJson(String json) {
+    final map = jsonDecode(json) as Map<String, dynamic>;
+    return ResponseModel(message: map['message'] as String);
+  }
+
+  final String message;
 }
 
 Future<String> _request(String url) async {
